d0e007c8ed55a39e2d72e626ab256118a78e1ff6
Raghed Idris <raghed.edriss@isae.edu.lb>
2021-07-01 18:03:38 +0300

    question3-3

--- tp4/question3/IHMCalculette.java
+++ tp4/question3/IHMCalculette.java
@@ -13,9 +13,11 @@ public class IHMCalculette extends JFrame {
         PileModele<Integer> modele = new PileModele<Integer>(new Pile2<Integer>(5));
         Controleur controle = new Controleur(modele);
         Vue vue = new Vue(modele);
+        //Vue2 vue2 = new Vue2(modele); //we can show both vues
 
         setLayout(new GridLayout(2, 1));
-        add(vue);
+        add(vue); 
+           // add(vue2); //add second vue
         add(controle);
         pack();
         setLocation(200,200);
